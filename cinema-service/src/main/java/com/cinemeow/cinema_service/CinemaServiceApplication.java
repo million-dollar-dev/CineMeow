3951e657681324baa89bc45de151669d8e2e289d
million-dollar-dev <hoangtuan1304@hotmail.com>
2025-08-13 22:43:15 +0700

    feat(be): cinema-service

---
+++ cinema-service/src/main/java/com/cinemeow/cinema_service/CinemaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cinemeow.cinema_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaServiceApplication.class, args);
+	}
+
+}
