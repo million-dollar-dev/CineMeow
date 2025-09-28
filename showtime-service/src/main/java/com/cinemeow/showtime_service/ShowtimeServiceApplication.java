45dc81d51b7af1060dacd537b968b5e90fd0823a
million-dollar-dev <hoangtuan1304@hotmail.com>
2025-09-28 16:26:54 +0700

    feat(be): init showtime-service

---
+++ showtime-service/src/main/java/com/cinemeow/showtime_service/ShowtimeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cinemeow.showtime_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowtimeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowtimeServiceApplication.class, args);
+	}
+
+}
