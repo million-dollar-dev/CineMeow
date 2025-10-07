526ca646f64056ecc0ffbefe7b3ca44d04ea123b
million-dollar-dev <hoangtuan1304@hotmail.com>
2025-10-07 15:25:50 +0700

    feat(be): init booking service

---
+++ booking-servicce/src/main/java/com/cinemeow/booking_servicce/BookingServicceApplication.java
@@ -0,0 +1,13 @@
+package com.cinemeow.booking_servicce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServicceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServicceApplication.class, args);
+	}
+
+}
