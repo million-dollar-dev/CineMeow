91a3e6d0d6ae19029fee9a725de651213401e773
million-dollar-dev <hoangtuan1304@hotmail.com>
2025-10-16 23:19:04 +0700

    feat(fe): call create promotion api

---
+++ admin-frontend/src/services/promotionService.js
@@ -0,0 +1,39 @@
+import { rootApi } from "./rootApi";
+
+const CONTEXT_PATH = 'promotion';
+
+export const brandApi = rootApi.injectEndpoints({
+    tagTypes: ["Promotions"],
+    endpoints: (builder) => ({
+        getAllPromotions: builder.query({
+            query: () => ({
+                url: `${CONTEXT_PATH}/promotions`,
+            }),
+            providesTags: ["Promotions"],
+        }),
+
+        createPromotion: builder.mutation({
+            query: (payload) => ({
+                url: `${CONTEXT_PATH}/promotions`,
+                method: "POST",
+                body: payload,
+            }),
+            invalidatesTags: ["Promotions"],
+        }),
+
+        updatePromotion: builder.mutation({
+            query: ({ id, ...payload }) => ({
+                url: `${CONTEXT_PATH}/promotions/${id}`,
+                method: "PUT",
+                body: payload,
+            }),
+            invalidatesTags: ["Promotions"],
+        }),
+    }),
+});
+
+export const {
+    useGetAllPromotionsQuery,
+    useCreatePromotionMutation,
+    useUpdatePromotionMutation
+} = brandApi;
