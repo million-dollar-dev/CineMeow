74f729e8f80951a0f6d010605839c87215405b24
million-dollar-dev <hoangtuan1304@hotmail.com>
2025-10-20 21:32:30 +0700

    refactor(be): promotion's condition handler

--- promotion-service/src/main/java/com/cinemeow/promotion_service/dto/request/VoucherValidationRequest.java
+++ promotion-service/src/main/java/com/cinemeow/promotion_service/dto/request/VoucherValidationRequest.java
@@ -1,9 +1,12 @@
 package com.cinemeow.promotion_service.dto.request;
 
+import com.cinemeow.promotion_service.dto.response.SeatResponse;
+import com.cinemeow.promotion_service.enums.RoomType;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder
@@ -16,8 +19,12 @@ public class VoucherValidationRequest {
     String userId;
     String showtimeId;
     String cinemaId;
-    List<String> seatIds;
+    String brandName;
+    RoomType roomType;
+    String paymentMethod;
+    List<SeatResponse> seats;
     List<String> fnbItemIds;
     BigDecimal totalPrice;
+    LocalDateTime bookingTime;
 }
 
