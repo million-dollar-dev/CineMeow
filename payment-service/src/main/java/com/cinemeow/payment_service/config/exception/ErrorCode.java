a55b54ef9107c6159d165b94d5f8624acbdc7673
million-dollar-dev <hoangtuan1304@hotmail.com>
2025-10-23 22:46:46 +0700

    feat(be): init payment-service

---
+++ payment-service/src/main/java/com/cinemeow/payment_service/config/exception/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.cinemeow.payment_service.config.exception;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public enum ErrorCode {
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    UNAUTHENTICATED(403, "Access denied", HttpStatus.UNAUTHORIZED)
+    ;
+
+    int code;
+    String message;
+    HttpStatusCode statusCode;
+}
